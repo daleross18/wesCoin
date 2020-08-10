ba91233cf68dc01d8fd19e118d943c14adc376ff
Dale Ross <daleross346@gmail.com>
2020-08-10 13:32:58 -0400

    Token Webpage Update v1.03

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 App = {
    web3Provider: null,
-
+   contracts: {};
    init: function() {
       console.log("App initialized...")
       return App.initWeb3();
@@ -17,6 +17,16 @@ App = {
          web3 = new Web3(App.web3Provider);
       }
    },
+
+   initContracts: function() {
+      $.getJSON("wesCoinSale.json", function(wesCoinSale) {
+         App.contracts.wesCoinSale = TruffleContract(wesCoinSale);
+         App.contracts.wesCoinSale.setProvider(App.web3Provider);
+         App.contracts.wesCoinSale.deployed().then(function(wesCoinSale){
+            console.log("wesCoin Sale Address:", wesCoinSale.address);
+         });
+      })
+   }
 }
 
 $(function() {
